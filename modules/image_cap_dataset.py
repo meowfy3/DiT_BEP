15d3f2177c478ef6c16340dcbf9963388400b96d
meowfy3 <168732996+meowfy3@users.noreply.github.com>
2024-05-22 15:00:18 +0200

    Update image_cap_dataset.py

--- modules/image_cap_dataset.py
+++ modules/image_cap_dataset.py
@@ -35,7 +35,7 @@ class ImgDataset(Dataset):
 
 
 if __name__ == '__main__':
-    img1, img2 = next(iter(ImgDataset("workspace/workspace/BEP256")))
+    img1, img2 = next(iter(ImgDataset(r'/workspace/BEP256')))
     plt.imshow(img1.permute(1, 2, 0))
     plt.show()
     plt.imshow(img2.permute(1, 2, 0))
