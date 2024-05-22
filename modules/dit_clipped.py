9b11862392c8114d2293ea789d255e43645c4f15
meowfy3 <168732996+meowfy3@users.noreply.github.com>
2024-05-22 15:01:01 +0200

    Update dit_clipped.py

--- modules/dit_clipped.py
+++ modules/dit_clipped.py
@@ -76,7 +76,7 @@ class DiT_Clipped(L.LightningModule):
         self.batch_size = batch_size
 
     def train_dataloader(self): # load the dataset
-        dataset = ImgDataset("workspace/workspace/BEP256")
+        dataset = ImgDataset(r'/workspace/BEP256')
         return DataLoader(
             dataset,
             batch_size=self.batch_size,
